b1f333497d5d97b00bbc01ef5f2f1b184dbea541
Luis Pimentel <37750879+lupimen@users.noreply.github.com>
2021-04-25 18:04:24 -0400

    Add files via upload

---
+++ build/precache-manifest.313d68374be59f78643c611883c3a200.js
@@ -0,0 +1,106 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "c9c911a5a58d46c8ae6a4d397c7c4a08",
+    "url": "/index.html"
+  },
+  {
+    "revision": "843e44f6aa673b1daf3a",
+    "url": "/static/css/3.00373544.chunk.css"
+  },
+  {
+    "revision": "73908d77e8ebb21c6aae",
+    "url": "/static/css/4.3e7d09e6.chunk.css"
+  },
+  {
+    "revision": "ddc1783fd3f58e8b20be",
+    "url": "/static/css/main.bc2002ce.chunk.css"
+  },
+  {
+    "revision": "b0521ff87653f9704da8",
+    "url": "/static/js/2.ab93319f.chunk.js"
+  },
+  {
+    "revision": "0a6d64cd3b49be6a14afdf21fa7c6d43",
+    "url": "/static/js/2.ab93319f.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "843e44f6aa673b1daf3a",
+    "url": "/static/js/3.d0a77490.chunk.js"
+  },
+  {
+    "revision": "73908d77e8ebb21c6aae",
+    "url": "/static/js/4.5535bc08.chunk.js"
+  },
+  {
+    "revision": "ddc1783fd3f58e8b20be",
+    "url": "/static/js/main.30d3b526.chunk.js"
+  },
+  {
+    "revision": "1f786b32cd707afe5cb5",
+    "url": "/static/js/runtime-main.c649cafa.js"
+  },
+  {
+    "revision": "21f233e19402cc4a66866a7f31191f0d",
+    "url": "/static/media/Agustina.21f233e1.woff"
+  },
+  {
+    "revision": "ee6539921d713482b8ccd4d0d23961bb",
+    "url": "/static/media/Montserrat-Regular.ee653992.ttf"
+  },
+  {
+    "revision": "47a7a717679b640ceb1b05d38c480a61",
+    "url": "/static/media/codeInLogo.47a7a717.webp"
+  },
+  {
+    "revision": "51cf7192327488da3d2a171280680c16",
+    "url": "/static/media/contactMailDark.51cf7192.svg"
+  },
+  {
+    "revision": "59389695208d7454c6607bed51ed4316",
+    "url": "/static/media/developerActivity.59389695.svg"
+  },
+  {
+    "revision": "e79c639294c805688be731921368c8f8",
+    "url": "/static/media/googleAssistantLogo.e79c6392.webp"
+  },
+  {
+    "revision": "ca146c42d5112599c8c2f4d47f111392",
+    "url": "/static/media/infosysLogo.ca146c42.png"
+  },
+  {
+    "revision": "52be31441c8cf8a8a16bd7a228d428bf",
+    "url": "/static/media/manOnTable.52be3144.svg"
+  },
+  {
+    "revision": "829ad0a1deef4d7295adfecdf12df2be",
+    "url": "/static/media/nextuLogo.829ad0a1.webp"
+  },
+  {
+    "revision": "23fb36bf15b8b49555af486e50bb478c",
+    "url": "/static/media/njLotteryLogo.23fb36bf.jpg"
+  },
+  {
+    "revision": "c737304db5dd5688b9c1ec5d3e90aff8",
+    "url": "/static/media/poshLogo.c737304d.png"
+  },
+  {
+    "revision": "ee62cb58630f29a6e6201b49d4c03728",
+    "url": "/static/media/pwaLogo.ee62cb58.webp"
+  },
+  {
+    "revision": "6f693cbce6d18cbc8b5e1476216fa528",
+    "url": "/static/media/rutgersLogo.6f693cbc.png"
+  },
+  {
+    "revision": "25018d29d5ab115bc51c3e9a2d92f90f",
+    "url": "/static/media/saayaHealthLogo.25018d29.webp"
+  },
+  {
+    "revision": "3440939881da8339d1597e8d77cdf850",
+    "url": "/static/media/skill.34409398.svg"
+  },
+  {
+    "revision": "13780cdd0144cacdef25486e2c2503f8",
+    "url": "/static/media/talksCardBack.13780cdd.svg"
+  }
+]);
